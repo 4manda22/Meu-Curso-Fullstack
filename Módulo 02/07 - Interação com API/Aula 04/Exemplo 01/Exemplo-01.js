93b325860105578c970828493bf157e19d3dd12c
Amanda <amandamikaelelira@gmail.com>
2024-02-05 09:15:54 -0300

    riky

---
+++ Módulo 02/07 - Interação com API/Aula 04/Exemplo 01/Exemplo-01.js
@@ -0,0 +1,11 @@
+let urlProdutos = "https://raw.githubusercontent.com/4manda22/Meu-Curso-Fullstack/master/M%C3%B3dulo%2002/07%20-%20Intera%C3%A7%C3%A3o%20com%20API/Consumir%20API/produtos.json"
+
+async function buscar(){
+    let resposta = await fetch(urlProdutos)
+    let produtos = await resposta.json()
+   // alert(produtos[1].descricao)
+    //console.log(produtos[1].descriao) 
+    
+    document.body.innerHTML = produtos[1].descricao
+}
+buscar()
