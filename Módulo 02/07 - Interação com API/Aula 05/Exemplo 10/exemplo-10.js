32fb4031afd97000a8cd6d4d8e850aa9dfcdcb6c
amanda <amandamikaelelira@gmail.com>
2024-02-19 11:06:15 -0300

    Update exemplo-10.js

--- Módulo 02/07 - Interação com API/Aula 05/Exemplo 10/exemplo-10.js
+++ Módulo 02/07 - Interação com API/Aula 05/Exemplo 10/exemplo-10.js
@@ -2,5 +2,13 @@ async function busca(){
     let procura = await fetch("lista-produtos.json")
     let produtos = await procura.jason()
 
-    
+    let listaDiv = document.getElementById("lista-card")
+
+    for(let x in produtos){
+        listaDiv.innerHTML = `<h1> ${produtos[x].nome} </h1>`
+
+    }
 }
+
+
+busca()
