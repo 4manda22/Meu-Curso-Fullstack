4909911877ae7364cd3d6048169beb4a7d17d7d2
amanda <amandamikaelelira@gmail.com>
2024-02-22 10:12:26 -0300

    update

---
+++ Módulo 02/07 - Interação com API/Aula 05/Exemplo 11/exemplo-02.js
@@ -0,0 +1,29 @@
+async function procura(){
+    let procura = await fetch("lista.json")
+    let produtos = await procura.json()
+
+    let listaDiv = document.getElementById("lista-card")
+
+    for(let produto of produtos){
+        listaDiv.innerHTML+= `
+            <div class= "card">
+                <img src="${produto.img}"
+                    width= "250" height= "250px">
+                <h3> ${produto.nome} </h3>
+                <p> ${produto.descricao} </p>
+                <div> 
+                    <span> R$ $ ${(produto.valorComDesconto).toFixed(2).replace("." , ",")} </span>
+                    <span> R$ $ ${(produto.valorSemDesconto).toFixed(2).replace("." , ",")}} </span>
+                </div>
+            </div>   
+            
+        `
+        
+    }    
+}       
+procura()
+   
+
+  
+
+
