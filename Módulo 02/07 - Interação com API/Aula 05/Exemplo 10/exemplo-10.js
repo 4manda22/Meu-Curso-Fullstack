6226b079d53140683a52f614480a050b80c77b59
amanda <amandamikaelelira@gmail.com>
2024-02-28 11:17:05 -0300

    gh

--- Módulo 02/07 - Interação com API/Aula 05/Exemplo 10/exemplo-10.js
+++ Módulo 02/07 - Interação com API/Aula 05/Exemplo 10/exemplo-10.js
@@ -7,29 +7,38 @@ async function busca(){
     for(let produto of produtos){
         listaDiv.innerHTML += `
             <div class="card" data-id="${produto.id}">
-                <img src="${produto.img}" 
-                    width="250" height="auto">
-                <h3> ${produto.nome} </h3>
-                <p> ${produto.descricao} </p>
-                <div>
-                    <span> R$ ${(produto.valorComDesconto).toFixed(2).replace("." , ",")} </span>
-                    <span> R$ ${(produto.valorSemDesconto).toFixed(2).replace("." , ",")} </span>
+                <div class="grupo-img"> 
+                    <img src="${produto.img}"  width="250" height="auto">
                 </div>
-            </div>
+                <div class= "textos">
+                     <h3> ${produto.nome} </h3>
+                    <p> ${produto.descricao} </p>
+                    </div>      
+                        <span class= "comDesconto"> R$ ${(produto.valorComDesconto).toFixed(2).replace("." , ",")} </span>
+                        <span class= "semDesconto"> R$ ${(produto.valorSemDesconto).toFixed(2).replace("." , ",")} </span>
+                    </div>
+                </div>   
+             </div>       
+                   
+                
+               
+                
+           
         `
 
     }
 
     let elementosCards = document.querySelectorAll(".card")
     for(let card of elementosCards){
-        card.addEventListener("click", clicou)
-    } 
+        card.addEventListener("click", cliquei)
+    }
 }
 
 busca()
 
-function clicou(){
-    let produtoId = this.getAttribute("data-id")
-    window.location.href = "detalhes.html?hahaha=" +  produtoId
+function cliquei(){
+    let elementosID = this.getAttribute("data-id")
+    (window.location.href)= "detalhes.html"
     
 }
+
